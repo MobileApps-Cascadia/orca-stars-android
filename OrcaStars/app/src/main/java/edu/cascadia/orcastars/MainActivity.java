5ffc57d31acce56f5e764ff4cbf38f5580a02069
Josh Martinez <joshuaaaronmartinez@gmail.com>
2019-11-18 13:16:07 -0800

    Closes 30 adds a search bar and settings feature to the actionbar

--- OrcaStars/app/src/main/java/edu/cascadia/orcastars/MainActivity.java
+++ OrcaStars/app/src/main/java/edu/cascadia/orcastars/MainActivity.java
@@ -1,14 +1,56 @@
 package edu.cascadia.orcastars;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.orca_stars48);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("OrcaStars");
+        toolbar.setSubtitle("by Orca Conservancy");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String msg=" ";
+        switch (item.getItemId()){
+            case R.id.search:
+                msg = "Search";
+                break;
+            case R.id.about:
+                msg = "About";
+                break;
+            case R.id.settings:
+                msg = "Settings";
+                break;
+        }
+        Toast.makeText(this,msg+ " Checked", Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
     }
 }
