94deadefa52b1215bdb4ce4ffcc51ca41e082002
Josh Martinez <joshuaaaronmartinez@gmail.com>
2019-11-18 16:49:12 -0800

    Closes 30
    Added elevation and other minor tweaks to the actionbar.

--- OrcaStars/app/src/main/java/edu/cascadia/orcastars/MainActivity.java
+++ OrcaStars/app/src/main/java/edu/cascadia/orcastars/MainActivity.java
@@ -1,15 +1,18 @@
 package edu.cascadia.orcastars;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationHost {
+
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +22,15 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setLogo(R.drawable.orca_stars48);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("OrcaStars");
-        toolbar.setSubtitle("by Orca Conservancy");
 
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new MenuFragment())
+                    .commit();
+        }
     }
 
     @Override
@@ -52,5 +61,25 @@ public class MainActivity extends AppCompatActivity {
         }
         Toast.makeText(this,msg+ " Checked", Toast.LENGTH_LONG).show();
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Navigate to the given fragment.
+     *
+     * @param fragment       Fragment to navigate to.
+     * @param addToBackstack Whether or not the current fragment should be added to the backstack.
+     */
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
     }
 }
