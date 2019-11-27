a226203f6211ae1c30551bd6b4574100521ba24c
Victoria <tazzygirl37@gmail.com>
2019-11-26 19:33:24 -0800

    Adds FishfoodX Restaurant main page

---
+++ OrcaStars/app/src/main/java/edu/cascadia/orcastars/FishfoodxFragment.java
@@ -0,0 +1,29 @@
+package edu.cascadia.orcastars;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FishfoodxFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fishfoodx, container, false);
+
+
+        // Snippet from "Navigate to the next Fragment" section goes here.
+
+        return view;
+    }
+}
