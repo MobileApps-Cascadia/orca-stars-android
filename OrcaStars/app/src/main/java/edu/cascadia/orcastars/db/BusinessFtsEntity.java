ed9e8ea2b83b666b85eb8819fc67bf925218cd49
Victoria <tazzygirl37@gmail.com>
2020-02-21 15:43:46 -0800

    Fixes all errors preventing the app from building

--- OrcaStars/app/src/main/java/edu/cascadia/orcastars/db/BusinessFtsEntity.java
+++ OrcaStars/app/src/main/java/edu/cascadia/orcastars/db/BusinessFtsEntity.java
@@ -9,7 +9,7 @@ public class BusinessFtsEntity{
     private String name;
     private int logo;
 
-    public BusinessFtsEntity(String name, int photo){
+    public BusinessFtsEntity(String name, int logo){
         this.name = name;
         this.logo = logo;
     }
@@ -18,7 +18,11 @@ public class BusinessFtsEntity{
         return name;
     }
 
+    public void setName(String name){this.name = name;}
+
     public int getLogo(){
         return logo;
     }
+
+    public void setLogo(int logo){this.logo = logo;}
 }
