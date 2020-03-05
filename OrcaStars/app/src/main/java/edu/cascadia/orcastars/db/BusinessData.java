a7660ff71dc971859026e68ed44d69c4215db43a
Victoria <tazzygirl37@gmail.com>
2020-03-04 20:35:07 -0800

    Updates business fragment with scrollview and spacing. Updates data with longer, more accurate business descriptions.

--- OrcaStars/app/src/main/java/edu/cascadia/orcastars/db/BusinessData.java
+++ OrcaStars/app/src/main/java/edu/cascadia/orcastars/db/BusinessData.java
@@ -22,17 +22,17 @@ public class BusinessData {
             "Ty's Thai Food"
     };
     private static final String[] BusDesc = new String[]{
-            "Best place ever!",
-            "We sell GREEN tea!",
-            "Totally not stuck...",
-            "Pixelated food makes zero waste!",
-            "There are too many.",
-            "We actually exist! Ooops!",
-            "MTT Brand!",
-            "The best place for sustainable food on the go!",
-            "We have many salads and teas!",
-            "Enjoy our brunch!",
-            "Ty Tie's Thai Treats."
+            "Fishfood X is dedicated to supporting sustainable fishing, and giving back by donating to the Orca Conservancy. All ingredients are 100% organic and ethically sourced.",
+            "The Green Bean is a tea and coffee shop that supports sustainable practices. Every item we provide is biodegradable or recyclable. No more water pollution!",
+            "StarStuck supports sustainable business and donates to non-profit organizations that help to clean out the water of plastic.",
+            "We here at Grillbits are determined to keep our environment and our orcas safe! All of our ingredients are also from sustainable farmers.",
+            "All of our fish are sourced only from sustainable fisheries, to keep the orcas fed and happy.",
+            "25% of all proceeds go to the Orca Conservancy, and other sustainable businesses that are working hard to protect our orcas.",
+            "Metatonic is determined to keep our orcas around! Everything is recyclable and reusable! No more plastic pollution!",
+            "RestStop #1 is the #1 place for sustainable food on the go! We also pride ourselves in using only reusable, recyclable, and completely biodegradable tableware!",
+            "We have many salads and teas! All of our veggies come from sustainable and zero-pollutions farms and orchards! Our salads are protecting the environment!",
+            "Jora's Brunch is proud of supplying only from local and sustainable businesses, and donating to the Orca Conservancy and their cause!",
+            "Ty Tie's Thai food is sustainable, cruelty-free, 100% organic, zero-waste, and biodegradable! We are proud protectors of our orca friends!"
     };
     private static final String[] BusHours = new String[]{
             "9am-9pm, Mon-Fri",
@@ -48,17 +48,17 @@ public class BusinessData {
             "9am-9pm, Mon-Fri",
     };
     private static final String[] BusLocation = new String[]{
-            "Google it.",
-            "Google it.",
-            "Google it.",
-            "Google it.",
-            "Google it.",
-            "Google it.",
-            "Google it.",
-            "Google it.",
-            "Google it.",
-            "Google it.",
-            "Google it.",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
+            "Bothell, WA",
     };
     private static final Integer[] BusLogo = new Integer[]{
             R.drawable.fishfood_x,
